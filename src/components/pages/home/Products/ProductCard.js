d4a9c0ae1247b2e279466aa5985a470821a4b130
Nancy <nancyd0716@gmail.com>
2022-08-20 11:02:35 +0530

    ProductDetail page added

--- src/components/pages/home/Products/ProductCard.js
+++ src/components/pages/home/Products/ProductCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Flex, Image } from 'components/atom';
 import { products } from 'components/helper/static-data';
 import * as S from './style';
 
 export default function ProductCard() {
+	const navigate = useNavigate();
 	return (
 		<Flex flexWrap="wrap" width="80%" ml="10px" height="20%">
 			{products?.map((item) => (
-				<S.Card key={item?.id}>
+				<S.Card key={item?.id} onClick={() => navigate('/product-detail')}>
 					<Image src={item?.image} width={220} height={232} />
 					<S.CardName>{item?.title}</S.CardName>
 					<Flex mt="10px">
--- src/route/index.js
+++ src/route/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import { ProductDetail } from 'components/pages/productDetail';
 
 export default function AppRoute() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/product-detail" element={<ProductDetail />} />
 			</Routes>
 		</BrowserRouter>
 	);
